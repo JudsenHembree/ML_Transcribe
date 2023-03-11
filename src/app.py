61fbad2a0bdd67e1b022a64ead6c63f3ab62a7df
Judsen <jhembre@g.clemson.edu>
2023-03-10 20:28:18 -0500

    Graphing (#6)
    * Many feats and graphing
    
    * reconfigure option for windows
    
    * reconfigure option for windows
    
    * please work from windows
    
    * reconf
    
    * fix path error
    
    * fix path error
    
    * removing subprocess
    
    * rmtree
    
    * autoscale
    
    * autoscale
    
    * autoscale
    
    * adding midi stuff
    
    * adding midi stuff
    
    * adding midi stuff
    
    * adding midi stuff
    
    * adding midi stuff
    
    * adding midi stuff

--- src/app.py
+++ src/app.py
@@ -19,45 +19,91 @@ def usage():
 
 def main():
     """Main function of the program"""
+
+    """Global variables"""
+    global GRAPH
+    global NEW
+    global CULL
+    global RECONF
+    global ML_Home
+
+    GRAPH = False
+    NEW = False
+    CULL = False
+    RECONF = False
+    ML_Home = "/ML_Transcribe"
+
+
+    """Parse command line arguments"""
     try:
-        opts, _ = getopt(sys.argv[1:], "hc:", ["help", "config="])
+        opts, _ = getopt(sys.argv[1:], "rhgndc:", ["reconfig", "help", "graph", "new", "delete", "config="])
     except GetoptError as err:
         print(err)
         usage()
         sys.exit(2)
     for opt, _ in opts:
-        if opt in ("-h", "--help"):
+        if opt in ("-r", "--reconfig"):
+            print("Reconfiguring.")
+            RECONF = True
+        elif opt in ("-h", "--help"):
             usage()
             sys.exit()
         elif opt in ("-c", "--config"):
             print("alt Config file not currently supported")
             sys.exit(2)
+        elif opt in ("-g", "--graph"):
+            print("Graphing all .wav files post split.")
+            GRAPH = True
+        elif opt in ("-n", "--new"):
+            print("Creating a new session folder.")
+            NEW = True
+        elif opt in ("-d", "--delete"):
+            print("Delete data in data_home folder")
+            CULL = True
         else:
-            print("Unknown option")
+            print("Unknown option" + opt) 
             usage()
             sys.exit(2)
-    config = utils.get_config()
-    print("Welcome to the music downloader!")
-    print("Enter the url of a spotify playlist or song")
-    url = input()
 
-    print("Name the folder you want to save the music to")
-    fold_end = input()
+    if RECONF:
+        ML_Home = utils.reconfigure()
+
+    config, file = utils.get_config()
+
+    if CULL:
+        utils.cull_data_home(config["data_home"])
+        utils.make_data_home(config["data_home"])
+    if NEW:
+        utils.attempt_archive(config["data_home"])
+        print("Welcome to the music downloader!")
+        print("Enter the url of a spotify playlist or song")
+        url = input()
+
+        print("Name the folder you want to save the music to")
+        fold_end = input()
 
 
-    session_folder, seperated_folder = utils.create_unique_folder(config["data_home"], fold_end)
-    spotdl = Spotdl(config["client_id"], config["client_secret"], output=session_folder)
-    songs = spotdl.search([url])
-    spotdl.download_songs(songs)
+        session_folder, seperated_folder = utils.create_unique_folder(config["data_home"], fold_end)
+        spotdl = Spotdl(config["client_id"], config["client_secret"], output=session_folder)
+        songs = spotdl.search([url])
+        spotdl.download_songs(songs)
 
-    songs_downloaded = glob(session_folder + "/*.mp3")
-    cmd = ["spleeter", "separate", "-o", seperated_folder, "-p", "spleeter:5stems"]
-    for song in songs_downloaded:
-        cmd.append(song)
-        try:
-            subprocess.run(cmd, check=True)
-        except subprocess.CalledProcessError as err:
-            print("Error: %s", str(err))
-        cmd.pop()
+        songs_downloaded = glob(session_folder + "/*.mp3")
+        cmd = ["spleeter", "separate", "-o", seperated_folder, "-p", "spleeter:5stems"]
+        for song in songs_downloaded:
+            cmd.append(song)
+            try:
+                subprocess.run(cmd, check=True)
+            except subprocess.CalledProcessError as err:
+                print("Error: %s", str(err))
+            cmd.pop()
+
+    if GRAPH:
+        active_folder = utils.get_active_folder(config["data_home"])
+        if active_folder is None:
+            print("No active folder found")
+            sys.exit(2)
+        utils.graph_all_wav_for_each_folder(active_folder)
+
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,103 @@
 """
 Utility functions for the project
 """
-import json
+from pathlib import Path as path
+from glob import glob
+from shutil import rmtree
+import sys
 import os
+import json
+import wave
+from matplotlib import pyplot as plt
+import pylab
+import numpy as np
+from basic_pitch.inference import predict
+from basic_pitch import ICASSP_2022_MODEL_PATH
+from visual_midi import Plotter
+from visual_midi import Preset
 
-def get_config(file='/ML_Transcribe/config/config.json'):
+
+def reconfigure():
+    """Reconfigures the config file"""
+    config, file = get_config()
+    print(config)
+    print(file)
+    pwd = os.getcwd()
+    print("Current data home is %s", str(config["data_home"]))
+    config["data_home"] = str(path(pwd + "/data"))
+    print("New data home is %s", str(config["data_home"]))
+    print("Current model directory is %s", str(config["MODEL_DIRECTORY"]))
+    config["MODEL_DIRECTORY"] = str(path(pwd + "/models"))
+    print("New model directory is %s", str(config["MODEL_DIRECTORY"]))
+
+    with open(file, 'w') as conf:
+        print(config)
+        json.dump(config, conf)
+
+def check_data_home(data_home):
+    """Checks if the data home has two folders"""
+    if len(os.listdir(data_home)) > 2:
+        print("Error: Data home has more than two folders")
+        print("Folders should be archive and the active folder")
+        sys.exit(2)
+
+def get_active_folder(data_home):
+    """Returns the active folder"""
+    check_data_home(data_home)
+    glob_path = path(data_home + "/*")
+    for folder in glob(str(glob_path)):
+        if os.path.isdir(folder):
+            if not folder.endswith("archive"):
+                print("Active folder is %s", folder)
+                return folder
+    return None
+
+def attempt_archive(data_home):
+    """Attempts to archive prior run into the archive folder"""
+    if not os.path.exists(data_home):
+        print("Warning: %s not found", data_home)
+        print("Creating data home")
+        os.makedirs(data_home)
+        return
+    glob_path = path(data_home + "/*")
+    dat = glob(str(glob_path))
+    print(dat)
+    if "archive" not in os.listdir(data_home):
+        print("Creating archive folder")
+        os.makedirs(os.path.join(data_home, "archive"))
+    for folder in dat:
+        if os.path.isdir(folder):
+            if not folder.endswith("archive"):
+                arcpath = os.path.join(data_home, "archive")
+                print("Archiving %s to %s", folder, path)
+                renamed = os.path.join(arcpath, os.path.basename(folder))
+                os.rename(folder, renamed)
+
+def get_config(file=path('/ML_Transcribe/config/config.json')):
     """Reads the config file and returns a dictionary"""
+    print("Reading config file")
+    if not os.path.exists(file):
+        print("Error: %s not found", file)
+        print("Warning: %s not found", file)
+        print("You were probably using a symlink")
+        print("Attempting to find config file")
+        pwd = os.getcwd()
+        if "ML_Transcribe" in pwd:
+            pwd = pwd[:pwd.find("ML_Transcribe")]
+            file = path(pwd + "ML_Transcribe/config/config.json")
+        else:
+            print("Error: Could not find config file")
+            sys.exit(2)
+    else:
+        print("Found config file at %s", file)
     with open(file, encoding="utf-8") as conf:
-        config = json.load(conf)
-    return config
+        print("Loading config file")
+        try:
+            config = json.load(conf)
+        except json.decoder.JSONDecodeError as err:
+            print("Error: %s", str(err))
+            sys.exit(2)
+    return config, file
 
 def create_unique_folder(data_home, fold_end):
     """Creates a unique folder for the session fold end is usually user input"""
@@ -19,3 +108,80 @@ def create_unique_folder(data_home, fold_end):
     if not os.path.exists(seperated_folder):
         os.makedirs(seperated_folder)
     return folder, seperated_folder
+
+def graph_all_wav_for_each_folder(folder):
+    """Graphs all .wav files in a folder Intended to be used on the seperated folder"""
+    # find seperated folder
+    for file in os.listdir(folder):
+        if file.endswith("seperated"):
+            for inner_file in os.listdir(os.path.join(folder, file)):
+                if os.path.isdir(os.path.join(folder, file, inner_file)):
+                    graph_all_wav(os.path.join(folder, file, inner_file))
+
+def graph_all_wav(folder):
+    """Graphs all .wav files in a folder"""
+    for file in os.listdir(folder):
+        print("Graphing %s", file)
+        if file.endswith(".wav"):
+            graph_wav(os.path.join(folder, file))
+            wav_to_midi(os.path.join(folder, file))
+
+def graph_wav(file):
+    """Graphs a .wav file"""
+    try:
+        wav = wave.open(file, 'r')
+    except FileNotFoundError:
+        print("Error: %s not found", file)
+        return
+    frames = wav.readframes(-1)
+    sound_info = np.fromstring(frames, 'int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    try:
+        # Plot the sound wave
+        fig = plt.figure(figsize=(10, 6), edgecolor='k')
+        plt.title("Wav Amplitude " + str(file))
+        plt.plot(sound_info)
+        plt.xlabel("Time")
+        plt.ylabel("Amplitude")
+        plt.autoscale(tight=True)
+        plt.savefig(file + "_amp.png")
+        plt.close(fig)
+
+        # Plot the spectrogram
+        pylab.figure(num=None, figsize=(19, 12))
+        pylab.subplot(111)
+        pylab.title("Spectrogram of " + str(file))
+        pylab.xlabel("Time")
+        pylab.ylabel("Frequency (Hz)")
+        pylab.specgram(sound_info, Fs=frame_rate)
+        pylab.savefig(file + "_spec.png")
+        pylab.close()
+    except ValueError:
+        print("Error: %s is empty", file)
+    except MemoryError:
+        print("Error: %s is too large", file)
+    except Exception as err:
+        print("Error: %s", str(err))
+
+def cull_data_home(data_home):
+    """Deletes the data home"""
+    rmtree(data_home)
+    make_data_home(data_home)
+
+def make_data_home(data_home):
+    """Makes the data home"""
+    if not os.path.exists(data_home):
+        os.makedirs(data_home)
+
+def wav_to_midi(wav_file):
+    """Converts a wav file to a midi file"""
+    print("Converting %s to midi", wav_file)
+    try:
+        model_output, midi_data, note_activations = predict(wav_file)
+    except Exception as err:
+        print("Error: %s", str(err))
+        return
+    print("Writing midi file")
+    midi_file = os.path.join(os.path.dirname(wav_file), os.path.basename(wav_file) + "_conv_midi.mid")
+    midi_data.write(midi_file)
